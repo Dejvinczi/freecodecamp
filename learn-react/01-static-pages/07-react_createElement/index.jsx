24f6b03a36df1ef40e2b6c3266a67f57bc5a4478
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-15 16:56:51 +0100

    feat(react-01): Update 07 name

--- learn-react/01-static-pages/07-react_createElement/index.jsx
+++
@@ -1,7 +0,0 @@
-import { createElement } from "react";
-import { createRoot } from "react-dom/client";
-
-const root = createRoot(document.getElementById("root"));
-const h1El = createElement("h1", null, "Hello from React!");
-
-root.render(h1El);
