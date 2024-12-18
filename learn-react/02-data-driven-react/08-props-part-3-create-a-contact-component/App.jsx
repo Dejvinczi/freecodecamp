5d9517f0724ebe1cd5fb903feeb52d3230012bac
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-18 16:38:40 +0100

    feat(react-02): Finish 08

---
+++ learn-react/02-data-driven-react/08-props-part-3-create-a-contact-component/App.jsx
@@ -0,0 +1,20 @@
+/**
+ * Challenge:
+ * - Create a Contact component in another file
+ * - Move one of the contact card articles below into that file
+ * - import and render 4 instances of that contact card
+ *     - Think ahead: what's the problem with doing it this way?
+ */
+import ContactCard from "./components/ContactCard";
+function App() {
+  return (
+    <div className="contacts">
+      <ContactCard />
+      <ContactCard />
+      <ContactCard />
+      <ContactCard />
+    </div>
+  );
+}
+
+export default App;
