606f3fc8e667408fb3dbf39ab376420dd37c1754
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-18 15:47:13 +0100

    feat(react-02): Finish 02

---
+++ learn-react/02-data-driven-react/02-travel-journal-header/components/Header.jsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header>
+      <div id="logo">
+        <img src="../globe.png" alt="Globe picture" />
+        <span>my travel journal.</span>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
