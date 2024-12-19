45beb6905fdc05b9a3e857b1927b18e1b92bbeff
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-19 16:39:05 +0100

    feat(react-03): Finish 07

---
+++ learn-react/03-react-state/07-props-vs-state-props/index.js
@@ -0,0 +1,12 @@
+function addTwoNumbers(a, b) {
+  a = 42;
+  return a + b;
+}
+
+console.log(addTwoNumbers(1, 2));
+
+function NavBar(props) {
+  props.logoIcon = "some-other-icon.png";
+}
+
+<Navbar logoIcon="spaatula.png" />;
