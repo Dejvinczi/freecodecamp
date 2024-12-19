588c8328d41d1fd5fbbfbc17ca3e1f2eee746be4
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-19 15:58:38 +0100

    feat(react-03): Finish 03

---
+++ learn-react/03-react-state/03-chef-claude-form/Main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <main>
+      <form className="add-ingredient-form">
+        <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient"></input>
+        <button>Add ingriedient</button>
+      </form>
+    </main>
+  );
+}
