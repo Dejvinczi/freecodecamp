77f93095c830661aee6b1c9fe0fd95b0afba112f
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-20 13:57:12 +0100

    feat(react-03): Finish 16

---
+++ learn-react/03-react-state/16-toggling-state/App.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+export default function App() {
+  const [isGoingOut, setIsGoingOut] = React.useState();
+
+  const toggleIsGoingOut = (e) => {
+    setIsGoingOut((prevState) => !prevState);
+  };
+
+  /**
+   * Challenge:
+   * - Initialize state for `isGoingOut` as a boolean
+   * - Make it so clicking the button flips that
+   *   boolean value (true -> false, false -> true)
+   * - Display "Yes" if `isGoingOut` is `true`, "No" otherwise
+   */
+
+  return (
+    <main>
+      <h1 className="title">Do I feel like going out tonight?</h1>
+      <button onClick={(e) => toggleIsGoingOut(e)} className="value">
+        {isGoingOut ? "Yes" : "No"}
+      </button>
+    </main>
+  );
+}
