add84644ce5f783c6c0a29c6ce1b2b8b433e124d
Dejvinczi <dejvinczi.dev@gmail.com>
2024-12-20 16:43:02 +0100

    feat(react-03): Finish 34

---
+++ learn-react/03-react-state/34-conditional-rendering-practice/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  const [unreadMessages, setUnreadMessages] = React.useState([]);
+
+  /**
+   * Challenge:
+   * Only display the <h1> below if there are unread messages
+   */
+
+  return <div>{!!unreadMessages.length && <h1>You have {unreadMessages.length} unread messages!</h1>}</div>;
+}
